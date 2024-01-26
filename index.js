20b880223b6c719b3c591f80b9829ae24219eafb
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-01-26 01:41:07 -0600

    added variable to fix .then statement.  Still getting errors in other places

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const shapes = require("./lib/shapes.js");
 const shapesTest = require("./lib/shapes.test.js");
+const createLogo = require("./lib/shapes");
 
 inquirer
     .prompt([
@@ -47,11 +48,15 @@ inquirer
     //throw error if not generated, also console.log message to user about colors 
     //otherwise console.log successful generation if it is generated
     //I'M NOT CALLING THE FUNCTION I WANT CORRECTLY
-    .then((response) => fs.writeFile("logo.svg", logoData,
-        (err) => err ? (console.error(err),
-            console.log("There was an error in generating your file.  Please check your color spelling, visit 147colors.com to reference available colors. Please try again!"))
+    .then((response) => {
+        //create new variable with response wrapped in createLogo function from other js page
+        const logoData = createLogo(response);
+        fs.writeFile("logo.svg", logoData,
+            (err) => err ? (console.error(err),
+                console.log("There was an error in generating your file.  Please check your color spelling, visit 147colors.com to reference available colors. Please try again!"))
 
-            : console.log("Generated logo.svg")))
+                : console.log("Generated logo.svg"))
+    })
 
 
 /*
